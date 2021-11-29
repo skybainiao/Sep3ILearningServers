4ad85289e38eae736a3bea1bc9099952242d0925
Chenben Tong <305594@via.dk>
2021-11-29 04:43:49 +0100

    asd

--- src/main/java/Model/Course.java
+++ src/main/java/Model/Course.java
@@ -1,6 +1,8 @@
 package Model;
 
-public class Course
+import java.io.Serializable;
+
+public class Course implements Serializable
 {
 
 
--- src/main/java/com/example/ILearningServer/PostSystem/CourseController.java
+++ src/main/java/com/example/ILearningServer/PostSystem/CourseController.java
@@ -5,11 +5,13 @@ import RMIClient.ClientImpl;
 import com.google.gson.Gson;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.sql.SQLException;
 
+@RestController
 public class CourseController
 {
 
@@ -28,6 +30,7 @@ public class CourseController
       throws SQLException, RemoteException
   {
     String str = gson.toJson(client.getCourses(courseName));
+    System.out.println(courseName);
 
     return str;
   }
